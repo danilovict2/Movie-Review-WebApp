d4d6d24b53d75e820e84535ac5bab329a516ff4c
danilovict2 <danilosindric@gmail.com>
2023-02-11 20:26:25 +0100

    finished home page frontend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" style="color: white;">
+        <div class="px-10">
+            <h1 class="text-warning pb-2">What to watch</h1>
+            <div class="WhatToWatch">
+
+                <h3 style="border-left: 3px solid yellow; border-radius: 3px; padding-left: 7px; margin-bottom: 30px;">
+                    <strong>Top picks</strong>
+                </h3>
+                <div class="row">
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                </div>
+            </div>
+            <div class="FromYourWatchlist">
+                <h3 style="border-left: 3px solid yellow; border-radius: 3px; padding-left: 7px; margin-bottom: 30px; margin-top: 30px;">
+                    <strong>From Your Watchlist</strong>
+                </h3>
+                <div class="row">
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                    <div class="col-2">
+                        <img class="w-100" src="https://upload.wikimedia.org/wikipedia/hr/8/8a/Dark_Knight.jpg">
+                        <div class="stars">stars</div>
+                        <div class="title">The Dark Knight</div>
+                        <div class="watchlist text-primary">+Watchlist</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>IMDb</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body class="bg-black">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-dark shadow-sm">
+            <div class="container">
+                <a class="navbar-brand bg-warning px-2" href="{{ url('/home') }}">
+                    <strong>IMDb</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+                        <form action="{{ url('search') }}" method="GET" role="search">
+                            <div class="input-group">
+                                <input type="search" placeholder="Search IMDb" class="form-control" size="50">
+                                <button type="submit" class="btn btn-primary px-1" style="width: 70px;">
+                                    Search
+                                </button>
+                            </div>
+                        </form>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item px-2 pt-2" style="color: white;">
+                            <strong>
+                                <a href="" class="link-light" style="text-decoration: none;">Watchlist</a>
+                            </strong>
+                        </li>
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre
+                                    style="color:white;">
+                                    <strong>{{ Auth::user()->name }}</strong>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a href="" class="dropdown-item">
+                                        Your Profile
+                                    </a>
+                                    <a href="" class="dropdown-item">
+                                        Your Ratings
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
