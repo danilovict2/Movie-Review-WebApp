0199a14a7d43867cccec97bc321ae619f22bf404
danilovict2 <danilosindric@gmail.com>
2023-02-25 18:37:18 +0100

    upd: made progress on review logic

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -11,11 +11,14 @@ class ReviewController extends Controller{
     }
 
     public function store(int $movie_id){
-
+        $data = request()->validate([
+            'star1' => 'required',
+        ]);
+        dd($data);
     }
 
     public function index(int $movie_id){
-        return view('movies.review');
+        return view('movies.review', compact('movie_id'));
     }
 
 }
--- resources/views/movies/review.blade.php
+++ resources/views/movies/review.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container" style="color: white;">
-        
+    <div class="container d-flex justify-content-center align-items-center" style="color: white;">
+        <form action="/review/{{$movie_id}}" method="POST">
+            @csrf
+
+            <div class="rating d-flex flex-row-reverse mb-2 pt-2">
+                @for ($i = 1; $i <= 10; ++$i)
+                    <input type="radio" class="" name="star{{$i}}" value="{{$i}}">
+                @endfor
+            </div>
+            <div class="submit d-flex justify-content-center pt-5">
+                <button type="submit" class="btn btn-primary btn-lg">
+                    Rate
+                </button>
+            </div>
+
+        </form>
     </div>
 @endsection
